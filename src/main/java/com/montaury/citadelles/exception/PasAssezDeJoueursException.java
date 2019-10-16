b9482ae62814715629c0ee9aa4d178af5f599f32
Damien Urruty <damien.urruty@legrand.fr>
2019-10-16 18:29:21 +0200

    Remove never used exception.

--- src/main/java/com/montaury/citadelles/exception/PasAssezDeJoueursException.java
+++
@@ -1,7 +0,0 @@
-package com.montaury.citadelles.exception;
-
-public class PasAssezDeJoueursException extends RuntimeException {
-    public PasAssezDeJoueursException() {
-        super("Pas assez de joueurs, veuillez en ajouter");
-    }
-}
